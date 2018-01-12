63fe68a5efc24677bb555616cab1321d112bf067
EemeliSaari <saari.eemeli@gmail.com>
2018-01-12 15:07:29 +0200

    Fine tuning
    Added comments and configured the structure of the class

--- process.py
+++ process.py
@@ -2,7 +2,6 @@ import math
 from multiprocessing import Pool, cpu_count
 
 import cv2
-import matplotlib.pyplot as plt
 import numpy as np
 
 from utils import norm
@@ -11,58 +10,67 @@ from regionprobs import RegionProbs as rp
 
 class ImageProcess:
 
-    def __init__(self, image, templates, fast_detector, brisk_detector, bf_detector, *modes):
+    def __init__(self, image, templates, fast_detector, brisk_detector, bf_detector, *args):
         """
         Constructor for ImageProcess object.
 
-        :param image:
-        :param templates:
-        :param fast_detector:
-        :param brisk_detector:
-        :param bf_detector:
-        :modes:
+        :param image: image to be analysed
+        :param templates: templates that we try to find from the image
+        :param fast_detector: cv2 Fast feature detector
+        :param brisk_detector: cv2 BRISK keypoint feature analyser
+        :param bf_detector: cv2 BruteForce feature matcher
         """
+        super().__setattr__('__dict__', {})
+        self.__dict__['image'] = image
+
         self.__templates = templates
         self.__fast = fast_detector
         self.__br = brisk_detector
         self.__bf = bf_detector
-        self.__image = image
         self.__detected = 0
-        self.__modes = modes
+        self.__modes = args
         self.__MAX_EUCLIDEAN = math.sqrt(195075)
 
         # normalizes the screen imgae
         self.__norm = norm.normalize_image(image)
-        # grayscale is used in all other analysis
+        # convert image to grayscale
         self.__gray = cv2.cvtColor(self.__norm, cv2.COLOR_RGB2GRAY)
-        # blurs the image using median blur method
+        # blurs the image using median blur method with kernel 7
         self.__blurred = cv2.medianBlur(self.__gray, 7)
         # extracts edges from image using Canny algorithm
         self.__edges = cv2.Canny(self.__blurred, 100, 200)
 
         self.analyse_image()
 
-    def get_image(self):
+    def __getattr__(self, key):
         """
-        Return the image
+        Gets class attribute
+        Raises AttributeError if key is invalid
         """
-        return self.__gray
+        if key in self.__dict__:
+            return self.__dict__[key]
+        else:
+            raise AttributeError
 
-    def get_detections(self):
+    def __setattr__(self, key, value):
         """
-        Returns detected objects
+        Sets class attribute according to value
+        If key was not found, new attribute is added
         """
-        return self.__detections
+        if key in self.__dict__:
+            self.__dict__[key] = value
+        else:
+            super().__setattr__(key, value)
 
     def analyse_image(self):
         """
         Analyses the image contours
         """
         stats = rp(self.__edges, mode='outer_full', output='struct').get_properties()
-        self.__detections = [x for x in stats if self.process_contour(x) == 1]
+        self.__dict__['detections'] = [x for x in stats if self.process_contour(x) == 1]
 
         if 'draw' in self.__modes:
-            for detection in self.__detections:
+            for detection in self.detections:
                 self.draw_box(detection)
 
     def process_contour(self, contour):
@@ -88,21 +96,16 @@ class ImageProcess:
             cnt = contour.cnt
             x, y, w, h = contour.bounding_box
             # draws a new masked image based on contour.
-            #print(contour.filled_image.shape)
-            #print(self.__gray[x:x+h, y:y+w].shape)
+
             mask = np.zeros(self.__gray.shape, np.uint8)
             cv2.drawContours(mask, [cnt], 0,255, -1)
-            #cv2.fillPoly(mask, pts=[cnt], color=(255,255,255))
-            # FAST points of interest analyze on the masked imge.
 
             masked = cv2.bitwise_and(self.__gray, mask)
 
+            # FAST points of interest analyze on the masked imge.
             kp = self.__fast.detect(masked[y:y+h,x:x+w], None)
-            #plt.imshow(masked[y:y+h,x:x+w], cmap='gray')
-            #plt.show()
-            GOOD_KP = 35
 
-            if len(kp) > GOOD_KP:
+            if len(kp) > 35:
 
                 for template in self.__templates:
 
@@ -123,6 +126,7 @@ class ImageProcess:
                         # takes the average color value in the contours area
                         mean = cv2.mean(self.__norm, mask=mask)
 
+                        # extract the features using brisk
                         kp, des = self.__br.compute(masked[y:y+h,x:x+w], kp)
                         # calculates the matches using the BF matcher.
                         matches = self.__bf.match(template.des, des)
@@ -142,27 +146,24 @@ class ImageProcess:
                             match[name] = 0.6
 
                     else:
-                        match[name] = self.__MAX_EUCLIDEAN
+                        match[name] = 1
 
                 # sorts the match dict
                 sorted_matches = sorted(match, key=lambda x:match[x])
 
-                goods = [match[x] for x in sorted_matches if match[x] < self.__MAX_EUCLIDEAN]
+                goods = [match[x] for x in sorted_matches if match[x] < 1]
 
                 if len(goods) > 2:
-                    print(match[sorted_matches[0]])
+
+                    # Checks the best match percentage and choose the name accordingly
                     if 0.1 > match[sorted_matches[0]] >= 0:
                         contour.name = sorted_matches[0]
                         detection += 1
 
-                    # if the best match is within 0 and 0.2 we detect that contour as most similiar to that template and
-                    # the second value is also somewhere close.
                     elif 0.20 > match[sorted_matches[0]] >= 0.1:
                         contour.name='m'
                         detection += 1
 
-                    # if it's somewhere between 0.2 and 0.8 it might be but the program can't tell with good enough
-                    # certainty so the text is '?'.
                     elif 0.20 <= match[sorted_matches[0]] <= 0.8:
                         contour.name='?'
 
@@ -178,6 +179,12 @@ class ImageProcess:
         """
         x, y, w, h = contour.bounding_box
 
-        cv2.rectangle(self.__image, (x, y), (x + w, y + h), (255, 255, 52), 2)
+        cv2.rectangle(self.image, (x, y), (x + w, y + h), (255, 255, 52), 2)
         font = cv2.FONT_HERSHEY_SIMPLEX
-        cv2.putText(self.__image, contour.name, (x - 5, y - 5), font, 0.9, (255, 255, 52), 1)
+        cv2.putText(self.image, contour.name, (x - 5, y - 5), font, 0.9, (255, 255, 52), 1)
+
+
+class AnalyseProcess:
+
+    def __init__(self):
+        pass
