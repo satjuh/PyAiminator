3083f8c59bc47609a9e5b3e2b4a627f7695509c3
EemeliSaari <saari.eemeli@gmail.com>
2018-01-14 20:51:26 +0200

    Reworked Judge
    Reworked Judge to only analyse collected data

--- judge.py
+++ judge.py
@@ -1,3 +1,4 @@
+import os
 from time import time
 
 import cv2
@@ -12,75 +13,48 @@ import object_detection.utils.visualization_utils as vis_util
 
 class Judge:
 
-    def __init__(self, detection, categories, templates, fast, br, bf):
+    def __init__(self, file_path):
         """
         Constructor for Judge class
 
         :param detection:
         :param categories:
         """
-        self.__detection = detection
-        self.__categories = categories
-        self.__templates = templates
-        self.__fast = fast
-        self.__br = br
-        self.__bf = bf
+        if os.path.exists(file_path):
+            self.__files = file_path
+        else:
+            raise OSError("File doesn not exist")
 
-    def analyse_screen(self, width, heigth):
+    def evaluate_tensorflow(self, detection, categories):
 
-        with self.__detection.as_default():
-            with tf.Session(graph=self.__detection) as sess:
+        with detection.as_default():
+            with tf.Session(graph=detection) as sess:
+                # Process all the files in selected directory
+                for file in os.listdir(self.__files):
 
-                print("Starting session.\n")
-                start = time()
-                while True:
-
-                    # captures the screen.
-                    screen = np.array(ImageGrab.grab(bbox=(0,27,800,627)))
-                    screen = cv2.cvtColor(screen, cv2.COLOR_RGB2BGR)
-
-                    intensity, kp = self.process_frame(screen)
-
-                    process_time = time()
-                    process = ip(screen, self.__templates, self.__fast, self.__br, self.__bf)
-                    process_time = time() - process_time
-
+                    image = cv2.imread(file)
                     # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
-                    image_np_expanded = np.expand_dims(screen, axis=0)
-                    image_tensor = self.__detection.get_tensor_by_name('image_tensor:0')
+                    image_np_expanded = np.expand_dims(image, axis=0)
+                    image_tensor = detection.get_tensor_by_name('image_tensor:0')
                     # Each box represents a part of the image where a particular object was detected.
-                    boxes = self.__detection.get_tensor_by_name('detection_boxes:0')
+                    boxes = detection.get_tensor_by_name('detection_boxes:0')
                     # Each score represent how level of confidence for each of the objects.
                     # Score is shown on the result image, together with the class label.
-                    scores = self.__detection.get_tensor_by_name('detection_scores:0')
-                    classes = self.__detection.get_tensor_by_name('detection_classes:0')
-                    num_detections = self.__detection.get_tensor_by_name('num_detections:0')
+                    scores = detection.get_tensor_by_name('detection_scores:0')
+                    classes = detection.get_tensor_by_name('detection_classes:0')
+                    num_detections = detection.get_tensor_by_name('num_detections:0')
 
                     # Actual detection.
                     (boxes, scores, classes, num_detections) = sess.run(
                         [boxes, scores, classes, num_detections],
                         feed_dict={image_tensor: image_np_expanded})
-                    # Visualization of the results of a detection.
-                    classes = np.squeeze(classes).astype(np.int32)
-                    print(self.__categories[classes[0]]['name'])
 
-                    print("Loop took {:} seconds.".format(time()-start))
+                    #classes = np.squeeze(classes).astype(np.int32)
+                    #print(categories[classes[0]]['name'])
 
-                    start = time()
+                    #TODO do something with the information
+                    #TODO save the results in to pandas dataframe
 
-                    cv2.imshow('AimAssistant', screen)
-
-                    if cv2.waitKey(25) & 0xFF == ord('q'):
-                        cv2.destroyAllWindows()
-                        break
-
-    def analyse_video(self, path, frames):
+    def evaluate_X(self):
         pass
 
-    def process_frame(self, image):
-        
-        gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-        intensity = cv2.mean(gray)[0]
-        kp = self.__fast.detect(gray, None)
-
-        return intensity, kp
