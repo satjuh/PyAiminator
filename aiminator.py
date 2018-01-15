182f8aef67eafd666cc63ae8e6482faeb82b5f7b
EemeliSaari <saari.eemeli@gmail.com>
2018-01-15 13:11:05 +0200

    Separated setups

--- aiminator.py
+++ aiminator.py
@@ -1,60 +1,10 @@
-import os
 import sys
-import tarfile
-import zipfile
 
 import cv2
-import numpy as np
-import tensorflow as tf
-import six.moves.urllib as urllib
 
-# Tensorflow object_detection imports
-import object_detection.utils.label_map_util as label_map_util
-
-from src import process, templates, judge
+from src import judge, process, templates
 
 NUM_CLASSES = 90
-
-def setup_tensor():
-    # Model to download.
-    MODEL_NAME = 'ssd_mobilenet_v1_coco_11_06_2017'
-    MODEL_FILE = MODEL_NAME + '.tar.gz'
-    DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
-
-    # List of the strings that is used to add correct label for each box.
-    PATH_TO_LABELS = os.path.join('object_detection', 'data', 'mscoco_label_map.pbtxt')
-
-    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-
-    # Downloads the model if it doesn't exist already
-    if not os.path.exists(MODEL_FILE):
-        print("Downloading the model...")
-        opener = urllib.request.URLopener()
-        opener.retrieve(DOWNLOAD_BASE + MODEL_FILE, MODEL_FILE)
-        tar_file = tarfile.open(MODEL_FILE)
-        for file in tar_file.getmembers():
-            file_name = os.path.basename(file.name)
-            if 'frozen_inference_graph.pb' in file_name:
-                tar_file.extract(file, os.getcwd())
-        print("Completed\n")
-
-    # Path to frozen detection graph. This is the actual model that is used for the object detection.
-    PATH_TO_CKPT = MODEL_NAME + '/frozen_inference_graph.pb'
-
-    # Loads the model 
-    detection_graph = tf.Graph()
-    with detection_graph.as_default():
-        od_graph_def = tf.GraphDef()
-        with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
-            serialized_graph = fid.read()
-            od_graph_def.ParseFromString(serialized_graph)
-            tf.import_graph_def(od_graph_def, name='')
-
-    label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
-    categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
-    category_index = label_map_util.create_category_index(categories)
-
-    return detection_graph, category_index
 
 
 def main():
@@ -80,7 +30,6 @@ def main():
 
     c = process.CollectProcess(ct_models, fast, br, bf, 'debug')
     c.collect_from_screen(800,600)
-    print(c)
 
 
 if __name__ == '__main__':
