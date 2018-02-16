746519ce5fb2d44a0bb5c75b8facc3f2e46a7a32
EemeliSaari <saari.eemeli@gmail.com>
2018-02-16 10:06:55 +0200

    Added more args, splitted main

--- aiminator.py
+++ aiminator.py
@@ -1,50 +1,78 @@
 import argparse
 import sys
+import warnings
 
 import cv2
 
-from src import judge, process, templates, setup
-from examples.screen_demo import live_demo 
+from examples.screen_demo import live_demo
 from examples.static_images import samples
+from src.judge import Judge
+from src.paths import DataPath
+from src.process import CollectProcess
+
+try:
+    from src.setup import setup_tensor
+except ImportError:
+    warnings.warn('Object_detection module not found')
+
+
+def collect(mode, w=800, h=600, lim=None):
+
+    print(w, h)
+
+    c = CollectProcess('debug')
+
+    if mode == 'video':
+        c.collect_from_video()
+    elif mode == 'screen':
+        c.collect_from_screen(w, h)
+
+
+def judge(clf):
+
+    print(clf)
+    j = Judge()
+
+    if clf == 'tensorflow':
+        dg, ci = setup.setup_tensor()
+        j.evaluate_tensorflow(dg, ci)
 
 
 def main():
     """
     Main function of the aiminator.py - captures the screen of the size 800x600 in
-    the top-left corner of the screen and feeds a new window to display with detected
-    models
+    the top-left corner of the screen and collects 100 samples to evaluate using judge function.
     """
-    # initialises fastfeaturedetector to get initial points of interests
-    fast = cv2.FastFeatureDetector_create(threshold=50, nonmaxSuppression=50)
+    collect('screen', lim=100)
+    judge('tensorflow')
 
-    # initialises Binary Robust Invariant Scalable Keypoints for
-    # keypoint descriptor analyze
-    br = cv2.BRISK_create()
-
-    # BruteForce matcher to compare matches
-    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-
-    # initialize the data of the templates
-    ct_models = templates.make_templates('images/templates/CT/', fast, br)
-
-    #dg, ci = setup.setup_tensor()
-    #data_path = "M:/Projects/PyAiminator"
-    data_path = ""
-    c = process.CollectProcess(ct_models, fast, br, bf, 'debug', path=data_path)
-    c.collect_from_screen(800, 600)
-    #j = judge.Judge(data_path + "data/data_0/", "df_0.csv")
-    #j.evaluate_tensorflow(dg, ci)
 
 if __name__ == '__main__':
     
     parser = argparse.ArgumentParser()
-    parser.add_argument('--demo')
+
+    parser.add_argument('--demo', help='Demo the process module. Available modes: live, samples')
+    parser.add_argument('--collect', help='Collect using method: screen, video')
+
+    parser.add_argument('-width', help='Screen width for collect process', type=int, default=800)
+    parser.add_argument('-height', help='Screen height for collect process', type=int, default=600)
+
+    parser.add_argument('--judge', help='Judge the collected data using: tensorflow, human')
 
     args = parser.parse_args()
 
-    if args.demo == 'live':
-        live_demo()
-    elif args.demo == 'samples':
-        samples('images/examples/')
-    else:
-        main()
+    try:
+        if len(sys.argv) > 1:
+            if args.demo == 'live':
+                live_demo()
+            elif args.demo == 'samples':
+                samples('images/examples/')
+            if args.collect:
+                collect(args.collect, w=args.width, h=args.height)
+            if args.judge:
+                judge(args.judge)
+        else:
+            main()
+
+    except argparse.ArgumentError:
+        print('Invalid arguments.')
