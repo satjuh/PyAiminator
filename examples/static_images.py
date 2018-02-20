de591cbf62663c871e7074ba0e4d9aefb9293097
EemeliSaari <saari.eemeli@gmail.com>
2018-02-20 11:10:46 +0200

    adjusted samples to use DataPath

--- examples/static_images.py
+++ examples/static_images.py
@@ -4,26 +4,23 @@ import cv2
 import matplotlib.pyplot as plt
 
 
-def samples(path):
-    # initialises fastfeaturedetector to get initial points of interests
-    fast = cv2.FastFeatureDetector_create(threshold=50, nonmaxSuppression=50)
-
-    # initialises Binary Robust Invariant Scalable Keypoints for
-    # keypoint descriptor analyze
-    br = cv2.BRISK_create()
-
-    # BruteForce matcher to compare matches
-    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+def samples():
 
     from src.process import ImageProcess as ip
     from src.templates import make_templates
+    from src.paths import DataPath
 
-    # initialize the data of the templates
-    ct_models = make_templates('images/templates/CT/', fast, br)
+    fast = cv2.FastFeatureDetector_create(threshold=50, nonmaxSuppression=50)
+    br = cv2.BRISK_create()
+    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
 
-    for file in os.listdir(path):
+    ct_models = make_templates('CT', fast, br)
 
-        image = cv2.imread(path + file)
+    dp = DataPath()
+
+    for file in os.listdir(dp.examples):
+
+        image = cv2.imread(os.path.join(dp.examples, file))
 
         process = ip(image, ct_models, fast, br, bf, 'draw')
 
